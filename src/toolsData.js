38d555797c0a94a158fdb0441e277c88db3ea305
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-28 20:26:15 +0100

    Update the image import in toolsData.js

--- src/toolsData.js
+++ src/toolsData.js
@@ -1,54 +1,65 @@
-const toolsData = [
-  { id: 1, toolName: "HTML5", logo: "src/assets/html_5.svg" },
+import htmlIcon from "/src/assets/html_5.svg";
+import cssIcon from "/src/assets/css_3.svg";
+import jsIcon from "/src/assets/js.svg";
+import tailwindIcon from "/src/assets/devicon_tailwindcss.svg";
+import reactIcon from "/src/assets/react.svg";
+import figmaIcon from "/src/assets/figma.svg";
+import framerIcon from "/src/assets/framer-svgrepo-com.svg";
+import miroIcon from "/src/assets/logos_miro-icon.svg";
+import webflowIcon from "/src/assets/skill-icons_webflow.svg";
+import procreateIcon from "/src/assets/procreate.svg";
 
-  { id: 2, toolName: "CSS3", logo: "src/assets/css_3.svg" },
+const toolsData = [
+  { id: 1, toolName: "HTML5", logo: htmlIcon },
+
+  { id: 2, toolName: "CSS3", logo: cssIcon },
 
   {
     id: 3,
     toolName: "JavaScript",
-    logo: "src/assets/js.svg",
+    logo: jsIcon,
   },
 
   {
     id: 4,
     toolName: "Tailwindcss",
-    logo: "src/assets/devicon_tailwindcss.svg",
+    logo: tailwindIcon,
   },
 
   {
     id: 5,
     toolName: "Reactjs",
-    logo: "src/assets/react.svg",
+    logo: reactIcon,
   },
 
   {
     id: 6,
     toolName: "Figma",
-    logo: "src/assets/figma.svg",
+    logo: figmaIcon,
   },
 
   {
     id: 7,
     toolName: "Framer",
-    logo: "src/assets/framer-svgrepo-com.svg",
+    logo: framerIcon,
   },
 
   {
     id: 8,
     toolName: "Miro",
-    logo: "src/assets/logos_miro-icon.svg",
+    logo: miroIcon,
   },
 
   {
     id: 9,
     toolName: "Webflow",
-    logo: "src/assets/skill-icons_webflow.svg",
+    logo: webflowIcon,
   },
 
   {
     id: 10,
     toolName: "Procreate",
-    logo: "src/assets/procreate.svg",
+    logo: procreateIcon,
   },
 ];
 
