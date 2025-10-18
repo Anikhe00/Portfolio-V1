62fe8b60e48b2e908ab330206b53badb4451ed0e
Shakirat Akanji <akanjishakirato@gmail.com>
2025-10-18 12:36:58 +0100

    Update the Portfolio Layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Projects from "./components/layout/Projects";
 import About from "./components/layout/About";
 import ToolSection from "./components/layout/ToolSection";
 import Articles from "./components/layout/Articles";
-import Navigation from "./components/layout/Navigation";
-import NavLink from "./components/common/NavLink";
 
 function App() {
   const sections = useMemo(
@@ -41,29 +39,16 @@ function App() {
   }, [sections]);
 
   return (
-    <div className="relative mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
-      <div className="flex flex-col lg:flex-row lg:justify-between gap-[2.5rem] lg:gap-[0.5rem]">
-        <Navigation sections={sections} active={active} />
-        <main
-          id="content"
-          className="flex flex-col gap-[2.5rem] lg:gap-[4rem] items-start lg:py-[4rem] w-full lg:w-[60%] overflow-visible"
-        >
-          <About />
-          <Projects />
-          <ToolSection />
-          <Articles />
-        </main>
-      </div>
-
-      <nav className="fixed w-auto py-[0.875rem] px-[1.5em] z-50 bg-gray-200 rounded-[1.5rem] lg:hidden bottom-4 left-1/2 -translate-x-1/2 flex gap-[0.75rem] items-center justify-center shadow-gray-50">
-        {sections.map((pageName) => (
-          <NavLink
-            key={pageName}
-            pageName={pageName}
-            isActive={active === pageName.toLowerCase()}
-          />
-        ))}
-      </nav>
+    <div className="relative mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 lg:px-80 md:py-16 lg:py-0">
+      <main
+        id="content"
+        className="flex flex-col gap-[2.5rem] lg:gap-[4rem] items-start lg:py-[4rem] w-full overflow-visible"
+      >
+        <About />
+        <Projects />
+        <ToolSection />
+        <Articles />
+      </main>
       <Analytics />
     </div>
   );
